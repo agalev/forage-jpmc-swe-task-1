0d7e112dac105d68145eed00ed1f9f2019f3ae17
Alexander Galev <alex.galev@yahoo.com>
2024-01-02 11:33:33 -0800

    getDataPoint correctly calculates average of bid/ask; getRatio correctly outputs the ratio of price_a and price_b

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,7 +8,7 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
@@ -16,9 +16,18 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
-
   """ ------------ Add more unit tests ------------ """
-
+  def test_getRatio_calculateRatio(self):
+    price_a = 120.48
+    price_b = 117.87
+    """Assertion for price_a > price_b"""
+    self.assertEqual(getRatio(price_a, price_b), price_a / price_b)
+  
+  def test_getRatio_calculateRatioPriceBZero(self):
+    price_a = 120.48
+    price_b = 0
+    """Assertion for price_b = 0"""
+    self.assertIsNone(getRatio(price_a, price_b))
 
 
 if __name__ == '__main__':
